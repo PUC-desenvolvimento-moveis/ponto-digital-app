cb87b491ccb8957eeaed70f3f9ac9d6aca0c1a3c
ign7 <quaresmaigor@yahoo.com>
2024-06-22 13:16:13 -0300

    (feat) funcao info total apropriacoe

--- lib/pages/total_horas.dart
+++ lib/pages/total_horas.dart
@@ -1,25 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'home.dart';
-import 'register.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';  // Para trabalhar com JSO
+import 'dart:convert';
 
-
-class TotalHorasApropriadasPage extends StatelessWidget {
+class TotalHorasApropriadasPage extends StatefulWidget {
   final String email;
 
   TotalHorasApropriadasPage({required this.email});
 
   @override
+  _TotalHorasApropriadasPageState createState() =>
+      _TotalHorasApropriadasPageState();
+}
+
+class _TotalHorasApropriadasPageState
+    extends State<TotalHorasApropriadasPage> {
+ 
+  late Future<String> _totalHorasFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _totalHorasFuture = Future.value("");
+    _getIdUsuario(widget.email); // Inicializa com um future vazio
+    _getTotalHoras();
+  }
+
+  Future<int> _getIdUsuario(String email) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token'
+    };
+    final response = await http.get(
+        Uri.parse('http://localhost:8000/api/users/byemail/$email'),
+        headers: headers);
+    final data = jsonDecode(response.body);
+    print(data);
+    return data['id'];
+  }
+
+  Future<String> _getTotalHoras() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token'
+    };
+    final idUsuario = await _getIdUsuario(widget.email);
+    final response = await http.get(
+        Uri.parse(
+            'http://localhost:8000/api/pontos/soma_minutos_trabalhados/$idUsuario'),
+        headers: headers);
+    final data = jsonDecode(response.body);
+    print(data);
+    return data['total_horas_trabalhadas'];
+  }
+
+  Future<List<dynamic>> _getListaApropriacoesByData() async { 
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token'
+    };
+
+    final idUsuario = await _getIdUsuario(widget.email);
+
+    final response = await http.get(
+        Uri.parse(
+            'http://localhost:8000/api/pontos/soma_minutos_trabalhados/$idUsuario'),
+        headers: headers);
+    final data = jsonDecode(response.body);
+    print(data);
+
+    if (data['lista_de_apropriacao'] is Map) {
+      // Se a lista de apropriações for um objeto, transforma em uma lista
+      return data['lista_de_apropriacao'].values.toList();
+    } else {
+      return data['lista_de_apropriacao'];
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Use o email para buscar os dados e renderizar a página
     return Scaffold(
       appBar: AppBar(
-        title: Text('Total de Horas Apropriadas'),
+        title: Text('Horas Apropriadas por Data'),
       ),
       body: Center(
-        child: Text('Email do usuário: $email'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Email do usuário: ${widget.email}'),
+            SizedBox(height: 20),
+            SizedBox(height: 20),
+            FutureBuilder<String>(
+              future: _totalHorasFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Erro ao buscar total de horas');
+                } else {
+                  return Text('Total de horas: ${snapshot.data}');
+                }
+              },
+            ),
+            SizedBox(height: 20),
+            FutureBuilder<List<dynamic>>(
+              future: _getListaApropriacoesByData(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Erro ao buscar lista de apropriações');
+                } else {
+                  List<dynamic> apropriacoes = snapshot.data!;
+                  return ListView(
+                    shrinkWrap: true,
+                    children: apropriacoes.map((apropriacao) {
+                      return Card(
+                        elevation: 3,
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        child: ListTile(
+                          title: Text('Apropriação ${apropriacao['id']}'),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                  'Data inicial: ${apropriacao['data_hora_inicial']}'),
+                              Text(
+                                  'Data final: ${apropriacao['data_hora_final']}'),
+                              Text('Tipo: ${apropriacao['tipo']}'),
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
